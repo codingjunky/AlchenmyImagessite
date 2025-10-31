f8611e785f804687b0254ef0a77e5219ce8283ad
Chris <crgotit@gmail.com>
2025-10-30 19:12:19 -0500

    Add AmpdUIKit component to page render

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
       <HowItWorks />
       <FeatureGrid />
       <UploadWorkspace />
+      <AmpdUIKit />
       <VideoPromptPanel />
       <ComingSoon />
       <Footer />
